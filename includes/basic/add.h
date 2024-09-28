f17beb3eb888235196e2d8790db21b84263286df
MelodicTechno <617179025@qq.com>
2024-09-28 12:54:41 +0800

    add lib add

---
+++ includes/basic/add.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace mtmath {
+int add(int a, int b);
+float add(float a, float b);
+char add(char a, char b);
+}
---
+++ src/basic/add.cpp
@@ -0,0 +1,18 @@
+#include "../../includes/basic/add.h"
+
+int mtmath::add(int a, int b)
+{
+    return a + b;
+}
+
+float mtmath::add(float a, float b)
+{
+    return a + b;
+}
+
+char mtmath::add(char a, char b)
+{
+    char _ = '0';
+    int mid = a - _;
+    return b + mid;
+}
